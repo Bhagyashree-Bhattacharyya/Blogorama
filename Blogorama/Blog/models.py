9f285face5db8b5d4b22cca1e4f71a54ef45e1d0
BhagyaShree <bbhattacharyya135@gmail.com>
2024-08-10 14:54:40 +0530

    started with models

--- Blogorama/Blog/models.py
+++ Blogorama/Blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Tag(models.Model):
+    value = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.value
