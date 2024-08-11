c90cbc6e48bf28b8b34ea9ba93c7d4d266bb9fe0
BhagyaShree <bbhattacharyya135@gmail.com>
2024-08-11 14:29:05 +0530

    custom template tags

--- Blogorama/Blog/views.py
+++ Blogorama/Blog/views.py
@@ -4,9 +4,10 @@ from Blog.models import Post
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
-    return render(request, "blog/index.html", {"posts": posts})
+    # posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.all()
+    return render(request, "Blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post":post})
+    return render(request, "Blog/post-detail.html", {"post":post})
